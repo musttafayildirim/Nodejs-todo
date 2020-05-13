7eeb97dde105537120040b2e3242967b76ff12aa
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2020-05-13 08:22:17 +0300

    heroku için port güncellendi.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var _ = require("underscore");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 
 /*database bağlantısı */
